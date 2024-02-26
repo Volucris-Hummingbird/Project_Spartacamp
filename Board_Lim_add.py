009418a1afb35037cfa4aa078b65819b745286e1
Volucris <urbanboy64@gmail.com>
2024-02-26 20:27:29 +0900

    project work proceed

--- Board_Lim_add.py
+++ Board_Lim_add.py
@@ -1,5 +1,5 @@
 import hashlib
-import secrets
+# import secrets
 
 # ----- 코드 정의 ------
 
@@ -9,24 +9,12 @@ class Member:
         self.name = name
         self.username = username
         self.password = self._encrypt_password(password)
-# hashlib 같은 경우는 완전히 이해하지 못하여 Chatgpt의 도움을 받았음.
 
     def _encrypt_password(self, password):
-        salt = secrets.token_hex(8)
-        hashed_password = hashlib.sha256(
-            (password + salt).encode()).hexdigest()
-        return {
-            "hashed_password": hashed_password,
-            "salt": salt
-        }
+        return hashlib.sha256(password.encode()).hexdigest()
 
-
-# # hashed password 용례
-# hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-
-    def display(Member):
-        print(f"이름은 {Member.name}, ID는 {Member.username}입니다.")
+    def display(self):
+        print(f"이름은 {self.name}, ID는 {self.username}입니다.")
         # # TODO : 코드 구현이 필요합니다.
         # pass
 
@@ -38,49 +26,77 @@ class post:
         self.content = content
         self.author = author
 
-    # def __str__(self):
-    #     return f"{self.title} {self.content} {self.author}"
-        # for self.author in author:
-        #     print(self.author)
-#     # TODO : 코드 구현이 필요합니다.
-#     pass
 
-
-Members = [
-    Member("Alice", "Alice1", 123456),
-    Member("Bob", "Bob1", 123456),
-    Member("Charlie", "Charlie1", 123456)
-]
-
-
-
-# for Member in Members:
-#     Member.display()
-#     print(f"Hashed password : {Member.password['hashed_password']}")
-#     print(f"Salt : {Member.password['salt']}")
-# 출력이 되지 않음. 뭐가 문제지?
-
+Members = []
 posts = []
+
+
+#  회원가입 기능과 같이 일정 기능을 수행하기 위해서는 '함수'를 만들어야 한다.
+#  더불어 input 기능에 대해서 숙지, 숙달해야한다.
+
+def create_member():
+    name = input("이름을 입력하세요 : ")
+    username = input("아이디를 입력하세요 : ")
+    password = input("비밀번호를 입력하세요 : ")
+    Members.append(Member(name, username, password))
+
+
+def create_post():
+    if not Members:
+        print("회원가입을 하셔야 게시글 작성이 가능합니다.")
+        return
+    author_username = input("게시글을 작성하기 위한 아이디를 입력해주세요 : ")
+    for Member in Members:
+        if Member.username == author_username:
+            title = input("글 제목을 입력하세요 : ")
+            contents = input("내용을 입력하세요 : ")
+            posts.append(post(title, contents, author_username))
+            print("글이 게시되었습니다.")
+            return
+    print("회원가입을 하셔야 게시글 작성이 가능합니다.")
+
+
+#  While true: 문에 대해서 숙달이 필요
+while True:
+    create_member_option = input("회원가입 하시겠습니까? (yes/no) : ")
+    if create_member_option.lower() == 'yes':
+        create_member()
+    else:
+        break
+
+while True:
+    create_post_option = input("게시글을 기재하겠습니까? (yes/no) : ")
+    if create_post_option.lower() == 'yes':
+        create_post()
+    else:
+        break
+
 for Member in Members:
-    for i in range(3):
-        post_title = f'{Member.name} 의 {i+1}번째 게시글 '
-        post_contents = f'이 게시글은 파이썬을 배우고자 하는 {i+1}번째 게시글입니다.'
-        posts.append(post(post_title, post_contents, Member.username))
+    print(Member.username)
 
 
-marked_user = "Alice1"
-# 특정 유저의 아이디를 입력
 for post in posts:
-    if post.author == marked_user:
+    if post.author == "Alice1":
         print(post.title)
 
-marked_content = "파이썬"
+
 for post in posts:
-    if marked_content in post_contents:
-        print(post.content)
+    if '파이썬' in post.content:
+        print(post.title)
+
+# marked_user = "Alice1"
+# # 특정 유저의 아이디를 입력
+# for post in posts:
+#     if post.author == marked_user:
+#         print(post.title)
+
+# marked_content = "파이썬"
+# for post in posts:
+#     if marked_content in post_contents:
+#         print(post.content)
 
 
-# # # ----- 코드 실행 ------
-# posts = []
+# # # # ----- 코드 실행 ------
+# # posts = []
 
-# # # TODO : 코드 구현이 필요합니다.
+# # # # TODO : 코드 구현이 필요합니다.
